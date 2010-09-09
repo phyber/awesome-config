4d1be08e5c1689c391648bff94d038399409c8ef
phyber <phyber@localhost>
2010-09-09 16:15:39 +0100

    More commas.

--- settings.lua
+++ settings.lua
@@ -8,7 +8,7 @@ modkey = "Mod4"
 theme = "/usr/share/awesome/themes/default/theme.lua"
 wallpaper = homedir .. "/.wallpaper/battlecry-dual-1680x1050.jpg"
 
-terminal = "uxterm",
-browser = "google-chrome",
+terminal = "uxterm"
+browser = "google-chrome"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
