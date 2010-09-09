acafc33e3de85e7bdf557ef47d3f1048064ea177
phyber <phyber@localhost>
2010-09-09 09:53:11 +0100

    Add first of many config files.

---
+++ rc.lua
@@ -0,0 +1,30 @@
+-- Fail safe, taken from github.com/bioe007
+
+require "awful"
+require "naughty"
+
+local confdir = awful.util.getdir("config")
+local rc, err = loadfile(confdir .. "/awesome.lua")
+if rc then
+	rc, err = pcall(rc)
+	if rc then
+		return
+	end
+end
+
+-- Loading our config failed, load the default!
+dofile("/etc/xdg/awesome/rc.lua")
+
+-- Notify us about the errors.
+for s = 1, screen.count() do
+	mypromptbox[s].text = awful.util.escape(err:match("[^\n]*"))
+end
+
+naughty.notify({
+	text = string.format(
+		"Awesome failed to start on %s. Error: %s\n",
+		os.date("%d/%m/%Y %T"),
+		err
+	),
+	timeout = 0
+})
