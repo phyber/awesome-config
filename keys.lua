72bce728d7e8d32ee176150890d7f7fa1015a3bb
phyber <phyber@localhost>
2010-09-10 15:00:35 +0100

    Oops, fix keys.lua. Forgot I wasn't in a function there.

--- keys.lua
+++ keys.lua
@@ -107,25 +107,29 @@ end
 
 -- Bind key numbers to tags
 for i = 1, keynumber do
-	local screen = mouse.screen
-	local tags = outline.tags
 	globalkeys = awful.util.table.join(globalkeys,
 		awful.key({ modkey }, "#".. i + 9, function()
+			local screen = mouse.screen
+			local tags - outline.tags
 			if tags[screen][i] then
 				awful.tag.viewonly(tags[screen][i])
 			end
 		end),
 		awful.key({ modkey, "Control" }, "#" .. i + 9, function()
+			local screen = mouse.screen
+			local tags = outline.tags
 			if tags[screen][i] then
 				awful.tag.viewtoggle(tags[screen][i])
 			end
 		end),
 		awful.key({ modkey, "Shift" }, "#" .. i + 9, function()
+			local tags = outline.tags
 			if client.focus and tags[client.focus.screen][i] then
 				awful.client.toggletag(tags[client.focus.screen][i])
 			end
 		end),
 		awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9, function()
+			local tags = outline.tags
 			if client.focus and tags[client.focus.screen][i] then
 				awful.client.toggletag(tags[client.focus.screen][i])
 			end
