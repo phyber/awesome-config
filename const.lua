d29996f70790e1838d888c9449752ac31b9e1bef
phyber <phyber@localhost>
2014-01-24 11:16:00 +0000

    Add const.lua

---
+++ const.lua
@@ -0,0 +1,12 @@
+--[[
+-- Constants module, magic numbers suck.
+--]]
+
+debugfile("Loading constants.")
+module("const")
+
+MOUSE_LEFT_BTN	= 1
+MOUSE_MIDDLE_BTN= 2
+MOUSE_RIGHT_BTN	= 3
+MOUSE_WHEEL_UP	= 4
+MOUSE_WHEEL_DN	= 5
